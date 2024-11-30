4cb63b24c468e2e639b1338b98d7439c76949cf9
João Felipe <joaofilipera13@gmail.com>
2024-11-30 14:11:38 -0300

    update

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from machine import Pin
 from neopixel import NeoPixel
 from time import sleep
+import _thread
 
-# Configuração do número de LEDs e cor
-num_pixels = 10
-roxo = (80, 200, 200)  # Cor azul
-rosa = (200, 200, 80)
-# Configuração do pino de dados da faixa NeoPixel
 strip_pin = Pin(0, Pin.OUT)
+pixels = 10
+strip = NeoPixel(strip_pin, pixels)
 
-# Inicialização da faixa NeoPixel
-my_strip = NeoPixel(strip_pin, num_pixels)
+def led():
+    while(True):
+        for i in range(pixels):
+            strip[i] = (255,0,0)
+            strip.write()
+            sleep(0.5)
+        for i in range(pixels):
+            strip[i] = (0,0,255)
+            strip.write()
+            sleep(0.5)
 
-while True:
-    for i in range(num_pixels):
-        my_strip.fill(rosa)  # Apaga todos os LEDs
-        my_strip[i] = roxo  # Acende o LED atual com a cor definida    
-        my_strip.write()  # Atualiza a faixa NeoPixel
-        sleep(0.3)  # Atraso de 100ms
+_thread.start_new_thread(led, ())
+
+led = Pin(25, Pin.OUT)
+while(True):
+    led.toggle()
+    sleep(1)
